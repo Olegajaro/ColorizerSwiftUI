d0d8d71ad8fce97d44b2736a8c698db1846acdb0
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-10-29 20:55:18 +0300

    First version

---
+++ ColorizerSwiftUI/ColorView.swift
@@ -0,0 +1,38 @@
+//
+//  ColorView.swift
+//  ColorizerSwiftUI
+//
+//  Created by Олег Федоров on 29.10.2021.
+//
+
+import SwiftUI
+
+struct ColorView: View {
+    
+    let redComponent: Double
+    let greenComponent: Double
+    let blueComponent: Double
+    
+    var body: some View {
+        Rectangle()
+            .foregroundColor(
+                Color(.sRGB,
+                      red: redComponent/255,
+                      green: greenComponent/255,
+                      blue: blueComponent/255,
+                      opacity: 1)
+            )
+            .frame(width: 380, height: 150)
+            .cornerRadius(20)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.white, lineWidth: 5)
+            )
+    }
+}
+
+struct ColorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorView(redComponent: 1, greenComponent: 255, blueComponent: 1)
+    }
+}
--- ColorizerSwiftUI/ContentView.swift
+++ ColorizerSwiftUI/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var redValue = Double.random(in: 1...255)
+    @State var greenValue = Double.random(in: 1...255)
+    @State var blueValue = Double.random(in: 1...255)
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color(white: 0.8)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 60) {
+                ColorView(
+                    redComponent: redValue,
+                    greenComponent: greenValue,
+                    blueComponent: blueValue
+                )
+                
+                VStack {
+                    ColorSliderView(sliderValue: $redValue)
+                    ColorSliderView(sliderValue: $greenValue)
+                    ColorSliderView(sliderValue: $blueValue)
+                }
+                
+                Spacer()
+            }
             .padding()
+            
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorSliderView: View {
+    
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            Text("\(lround(sliderValue))")
+                .frame(width: 50)
+            Slider(value: $sliderValue, in: 1...255, step: 1)
+            TextField("", text: .constant("\(lround(sliderValue))"))
+                .textFieldStyle(.roundedBorder)
+                .frame(width: 50)
+        }
     }
 }
