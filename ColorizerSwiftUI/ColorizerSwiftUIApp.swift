57337299b2df81f6ed92d978d027094af293a758
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-10-29 19:37:22 +0300

    Initial Commit

---
+++ ColorizerSwiftUI/ColorizerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizerSwiftUIApp.swift
+//  ColorizerSwiftUI
+//
+//  Created by Олег Федоров on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorizerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
